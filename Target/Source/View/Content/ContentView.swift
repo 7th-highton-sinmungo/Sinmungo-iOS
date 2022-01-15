81add2cf12e8197bd710540e5f34d1d1644b4a37
prs176 <prsx176@gmail.com>
2022-01-15 17:57:40 +0900

    [MODIFY] tab view

--- Target/Source/View/Content/ContentView.swift
+++ Target/Source/View/Content/ContentView.swift
@@ -14,9 +14,17 @@ struct ContentView: View {
         TabView(selection: $selection) {
             MainView()
                 .tabItem {
-                    Image(systemName: selection == 0 ? "house" : "house.fill")
+                    Image(systemName: "house")
+                    Text("홈")
                 }
                 .tag(0)
+            
+            UserView()
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("내 정보")
+                }
+                .tag(1)
         }
     }
 }
