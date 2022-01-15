5ff804ab6b83b051476adaf1e8e5850bcb8b3bb1
baegteun <baegteun@gmail.com>
2022-01-15 16:44:39 +0900

    :sparkles: :: UIFrame
    화면 크기 struct 추가

---
+++ Target/Source/Presentation/Utils/Common/UIFrame/UIFrame.swift
@@ -0,0 +1,15 @@
+//
+//  UIFrame.swift
+//  Sinmungo
+//
+//  Created by 최형우 on 2022/01/15.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+import UIKit
+
+struct UIFrame{
+    static let bound = UIScreen.main.bounds
+    static let width = bound.width
+    static let height = bound.height
+}
