3dd363c07ec7194c1a684615b839e4a8f6b026c1
prs176 <prsx176@gmail.com>
2022-01-15 23:18:19 +0900

    [ADD] 좋아요한 게시물 보기

--- Target/Source/Presentation/View/PreferredPost/PreferredPostView.swift
+++ Target/Source/Presentation/View/PreferredPost/PreferredPostView.swift
@@ -1,16 +1,17 @@
 //
-//  MyPostsView.swift
+//  PreferredPostView.swift
 //  Sinmungo
 //
-//  Created by 최형우 on 2022/01/15.
+//  Created by 박세은 on 2022/01/15.
 //  Copyright © 2022 baegteun. All rights reserved.
 //
 
 import SwiftUI
 
-struct MyPostsView: View {
-    @StateObject var viewModel = MyPostsViewModel()
+struct PreferredPostView: View {
+    @StateObject var viewModel = PreferredPostViewModel()
     @Environment(\.presentationMode) var mode: Binding<PresentationMode>
+    
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
@@ -21,13 +22,13 @@ struct MyPostsView: View {
             .padding()
         }
         .configureBackbutton(mode: mode)
-        .navigationTitle("MY")
+        .navigationTitle("Preferred")
         .navigationBarTitleDisplayMode(.automatic)
     }
 }
 
-struct MyPostsView_Previews: PreviewProvider {
+struct PreferredPostView_Previews: PreviewProvider {
     static var previews: some View {
-        MyPostsView()
+        PreferredPostView()
     }
 }
