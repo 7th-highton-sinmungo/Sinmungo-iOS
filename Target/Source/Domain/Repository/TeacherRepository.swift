8bc7f57e2e7051fddb0d3330b50811bdbfa802a7
baegteun <baegteun@gmail.com>
2022-01-16 01:29:39 +0900

    :sparkles: :: TeacherRepository

---
+++ Target/Source/Domain/Repository/TeacherRepository.swift
@@ -0,0 +1,15 @@
+//
+//  TeacherRepository.swift
+//  Sinmungo
+//
+//  Created by 최형우 on 2022/01/16.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+import Combine
+
+protocol TeacherRepository{
+    func applyRegister(req: TeacherRegisterRequest) -> AnyPublisher<TokenResponse, Error>
+    
+    func fetchTeacherInfo() -> AnyPublisher<TeacherInfoResponse, Error>
+}
