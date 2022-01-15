360f3995f990cf80418c45fc921e03a765bd0ce1
prs176 <prsx176@gmail.com>
2022-01-16 02:24:34 +0900

    [ADD] fetch posts

--- Target/Source/Presentation/VIewModel/Main/MainViewModel.swift
+++ Target/Source/Presentation/VIewModel/Main/MainViewModel.swift
@@ -8,12 +8,20 @@
 
 import Foundation
 
-class MainViewModel: ObservableObject {
+class MainViewModel: BaseViewModel {
     @Published var posts: [Post] = [Post(index: 0, content: "하겐다즈", createdAt: Date(), images: ["https://file.mk.co.kr/meet/neds/2020/10/image_readtop_2020_1043615_16024868354388303.jpg", "https://img.insight.co.kr/static/2019/10/08/700/tvwbg5830h6shlqi2220.jpg"], likeCount: 20, isLike: true, isChosen: false, isMine: false)]
     @Published var searchType = SearchType.POPULAR
+    
+    let userRemote = UserRemote()
+    
+    func fetchPosts() {
+        addCancellable(userRemote.getSortedPosts(searchType.rawValue)) { [weak self] posts in
+            self?.posts = posts
+        }
+    }
 }
 
-enum SearchType {
-    case POPULAR
-    case RECENT
+enum SearchType: Int {
+    case RECENT = 0
+    case POPULAR = 1
 }
