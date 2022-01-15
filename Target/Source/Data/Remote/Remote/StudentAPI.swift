b4a9cd06b3864722d268448c0ab5b4945de31966
prs176 <prsx176@gmail.com>
2022-01-15 23:46:55 +0900

    [ADD] student api

---
+++ Target/Source/Data/Remote/Remote/StudentAPI.swift
@@ -0,0 +1,59 @@
+//
+//  StudentAPI.swift
+//  Sinmungo
+//
+//  Created by 박세은 on 2022/01/15.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+import Moya
+
+enum StudentAPI {
+    case postRegister(request: StudentRegisterRequest)
+    case getUserInfo
+}
+
+extension StudentAPI: TargetType {
+    var baseURL: URL {
+        return URL(string: Constants.DEFAULT_HOST + "student")!
+    }
+    
+    var path: String {
+        switch self {
+        case .postRegister:
+            return "/"
+        case .getUserInfo:
+            return "/info"
+        }
+    }
+    
+    var method: Method {
+        switch self {
+        case .postRegister:
+            return .post
+        case .getUserInfo:
+            return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case let .postRegister(request):
+            return .requestData(try! JSONEncoder().encode(request))
+        case .getUserInfo:
+            return .requestPlain
+        }
+    }
+    
+    var sampleData: Data {
+        return Data()
+    }
+    
+    var headers: [String : String]? {
+        var headers = ["Content-Type": "application/json"]
+        headers["authorization"] = "Bearer \(TokenController.getInstance().getToken())"
+        
+        return headers
+    }
+}
+
---
+++ Target/Source/Data/Remote/Request/StudentRegisterRequest.swift
@@ -0,0 +1,29 @@
+//
+//  StudentRegisterRequest.swift
+//  Sinmungo
+//
+//  Created by 박세은 on 2022/01/15.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+import Foundation
+
+class StudentRegisterRequest: Codable {
+    var id: String
+    var password: String
+    var name: String
+    var grade: Int
+    var classNum: Int
+    var number: Int
+    var profileImageUrl: String
+    
+    enum StudentRegisterRequestKeys: String, CodingKey {
+        case id = "id"
+        case password = "password"
+        case name = "name"
+        case grade = "grade"
+        case classNum = "class_num"
+        case number = "number"
+        case profileImageUrl = "profile_image_url"
+    }
+}
