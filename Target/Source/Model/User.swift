fbd6389513935f1f05e9e99bdcfb32a72b5bc5fd
prs176 <prsx176@gmail.com>
2022-01-15 18:09:34 +0900

    [ADD] user model

---
+++ Target/Source/Model/User.swift
@@ -0,0 +1,46 @@
+//
+//  User.swift
+//  Sinmungo
+//
+//  Created by 박세은 on 2022/01/15.
+//  Copyright © 2022 baegteun. All rights reserved.
+//
+
+import Foundation
+
+struct StudentUser: Codable {
+    var name: String
+    var gcn: Int
+    var profileImageUrl: String
+    
+    // Codable
+    enum StudentUserKeys: String, CodingKey {
+        case name = "name"
+        case gcn = "gcn"
+        case profileImageUrl = "profile_image_url"
+    }
+    
+    // General
+    internal init(name: String, gcn: Int, profileImageUrl: String) {
+        self.name = name
+        self.gcn = gcn
+        self.profileImageUrl = profileImageUrl
+    }
+}
+
+struct TeacherUser: Codable {
+    var name: String
+    var profileImageUrl: String
+    
+    // Codable
+    enum StudentUserKeys: String, CodingKey {
+        case name = "name"
+        case profileImageUrl = "profile_image_url"
+    }
+    
+    // General
+    internal init(name: String, profileImageUrl: String) {
+        self.name = name
+        self.profileImageUrl = profileImageUrl
+    }
+}
